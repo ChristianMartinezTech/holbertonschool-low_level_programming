1ec23984319faa0352c35b519fdc0a6d9329567e
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-04 01:21:57 +0000

    Added a second asterisk at the start of the notes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
-/*
- * Write a C program that prints exactly
- * "Programming is like building a multilingual puzzle,
- * followed by a new line.
+/**
+ * main - Entry point
  *
- * Use the function puts
- * You are not allowed to use printf
- * Your program should end with the value 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
