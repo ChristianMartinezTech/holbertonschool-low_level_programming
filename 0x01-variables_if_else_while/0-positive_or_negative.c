daab4f16fef1286e881be75c9737a3195244e09b
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-04 20:42:45 +0000

    Added backslashn

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,11 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 	else if (n > 0)
 	{
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 			}
 	return (0);
 }
