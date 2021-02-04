0db46baf552c26fba65b75657ba88dff33540262
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-04 23:02:50 +0000

    Coded 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+		putchar(abc);
+	return (0);
+}
