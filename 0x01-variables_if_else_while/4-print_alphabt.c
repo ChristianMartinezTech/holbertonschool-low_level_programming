d7f95b8b27d7ff453205c69f55fcbe1dd437213a
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-05 01:08:18 +0000

    Coded 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+		if (abc != 'e' && abc != 'q')
+		putchar(abc);
+	putchar('\n');
+	return (0);
+}
