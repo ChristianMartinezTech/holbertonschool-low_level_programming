026d28b9cddfe765d10a4d45f4f980fdef9dd69f
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-06 02:44:45 +0000

    Coded 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-
 /**
- * main - Print the alphabt
+ * main - Print the numbers
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int a;
 
-	for (a = '0'; a <= 'f'; a++)
+	for (a = 0; a < 10; a++)
+		putchar(a + 48);
+	for (a = 'a'; a <= 'f'; a++)
 		putchar(a);
 	putchar('\n');
 	return (0);
+	
 }
