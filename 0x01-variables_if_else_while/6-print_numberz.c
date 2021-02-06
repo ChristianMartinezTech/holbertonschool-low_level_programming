04cfa396058424fc3c53d312b8a3d4710c696a2b
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-06 01:37:06 +0000

    Coded 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabt
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
