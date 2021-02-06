bfd37842281e057b79b920b5ba5a707850c72d4e
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-06 02:07:25 +0000

    Coded 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char z;
+
+	for (z = 'z'; z >= 'a'; z++)
+		putchar(z);
+	putchar('\n');
+	return (0);
+}
