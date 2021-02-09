952e22b15f78c0374d7956d2fe15e167efd8e050
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-09 03:52:33 +0000

    Check on the header

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+		_putchar(a);
+	_putchar('\n');
+}
