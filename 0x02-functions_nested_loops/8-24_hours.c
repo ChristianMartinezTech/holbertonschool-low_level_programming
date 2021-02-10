b7f50d94121a586ac864eb64c3c2ed4796cb93e6
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-10 00:57:58 +0000

    Coded 7 and 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * jack_bauer - print every minute
+ *
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
