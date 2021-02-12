bb11124c043ece0dc0a0f843c7e0429b87830866
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-12 01:07:52 +0000

    Coded 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print numbers using _putchar
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+}
