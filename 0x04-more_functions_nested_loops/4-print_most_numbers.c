7fa021fbff37c7b874783dc470f940814a070777
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-12 02:39:06 +0000

    Coded 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print numbers using _putchar
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != '2' || a != '4')
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
