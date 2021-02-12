78adfaf7dbcf4a59fe20367cc2045fa0e79f17e9
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-12 00:31:56 +0000

    Coded 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - returns 1 for an uppercase character, 0 if its lowercase
+ * @a: variable
+ * @b: variable
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
