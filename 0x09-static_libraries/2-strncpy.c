a4040e37179cb43592ebf045d1fd6f077316027d
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-27 02:01:30 +0000

    Created all the 1-2-

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - funtion
+ * @dest: var
+ * @src: var
+ * @n: var
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _puts - funtion
+ * @str: var
+ * Return: NULL
+ */
+
+void _puts(char *str)
+{
+}
