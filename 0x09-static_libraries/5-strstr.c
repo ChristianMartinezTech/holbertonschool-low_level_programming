538a60d2ba9802f76549b962fcbf5d2158755bc9
ChristianMartinezTech <2388@holbertonschool.com>
2021-02-27 18:41:35 +0000

    All files created and static lib created

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _strstr - funtion
+ * @haystack: var
+ * @needle: var
+ * Return: char 
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - funtion
+ * @dest: var
+ * @src: var
+ * Return: char 
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+}
