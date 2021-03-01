0b4dba47bd254a8d8e1cb5c978aad45cfb197708
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-01 21:48:54 +0000

    Changed the holberton.h

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * function_name - prints the name of the program
+ * main - prints the name of the program
  * @argc: var
  * @argv: var
  * Return: 0
  */
 
-int function_name(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 		printf("%s\n", argv[0]);
 		return (0);
--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -1,6 +1,4 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 int _putchar(char c);
-int function_name(int argc __attribute__((unused)), char *argv[]);
-int numbr_arguments(int argc, char *argv[]);
 #endif
