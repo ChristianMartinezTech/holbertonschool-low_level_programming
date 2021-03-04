3cdddc5e204a83bc86739c7a1d1f8e4053568527
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-04 01:46:27 +0000

    Coded 0, still getting BEtty issues

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - prints the name of the program
+ * @size: var
+ * @c: var
+ * Return: 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+
+	unsigned int a;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		ptr = malloc(sizeof(char) * size);
+		if (ptr == NULL)
+			{
+				return (NULL);
+			}
+
+		for (a = 0; a < size; a++)
+		{
+			ptr[a] = c;
+		}
+		return (ptr);
+	}
+}
