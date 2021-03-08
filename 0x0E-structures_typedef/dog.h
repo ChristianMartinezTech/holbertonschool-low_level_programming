c6816b92a4c68edf26f8aef931004f4b209a4933
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-08 20:55:21 +0000

    Coded 1 wiating for checker feedback

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DEG_H
+
+/**
+ * struct dog - struct with dates.
+ * @name: name.
+ * @age: age.
+ * @owner: owner.
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
