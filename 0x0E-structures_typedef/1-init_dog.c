e86466e3e5008b67e8578f4f0473315572cc535a
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-09 00:43:39 +0000

    Fixed betty issue with trailing whitespace

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,8 +13,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != '\0')
-		
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 }
