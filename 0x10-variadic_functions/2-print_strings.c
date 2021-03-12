ff9448abfc2e4acc5cddbe56233ad49ba4e0d245
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-12 01:36:29 +0000

    Started writting 2, will keep on when whiteboarded this exercise

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - sum function.
+ * @n: number.
+ * @separator: var.
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	
+}
