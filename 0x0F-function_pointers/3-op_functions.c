9067199c171140819527023937d34236fd4fd14e
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-13 01:03:31 +0000

    Coded

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+#include "3-calc.h"
+/**
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* @a: first number
+* @b: second number
+* Return: int
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * @a: first number
+ * @b: second number
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	(a % b);
+}
