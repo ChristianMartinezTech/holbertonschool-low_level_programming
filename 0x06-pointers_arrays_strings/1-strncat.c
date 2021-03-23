691146037a8161fd6ba84e547c9198352e136283
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-23 21:48:51 +0000

    Coded 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strncat - check the code for Holberton School students.
+ * @dest: var
+ * @src: var
+ * @n: var
+ * Return: returns a pointer to a char.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+		for (a = 0; dest[a] != '\0'; a++)
+		{
+		}
+		for (b = 0; b < n && src[b] != '\0'; b++)
+		{
+		dest[a + b] = src[b];
+		dest[a + b] = '\0';
+		}
+		return (dest);
+}
