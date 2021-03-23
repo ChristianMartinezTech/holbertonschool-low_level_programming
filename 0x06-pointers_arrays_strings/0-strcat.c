9bd98a2a518cfe955a09b099605ee7445ceba308
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-23 21:18:50 +0000

    Coded 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,9 +2,23 @@
 
 /**
  * *_strcat - main function
- *
- * Return 0 "Strings"
+ * @dest: pointer to s1
+ * @src: pointer to s2
+ * Return: 0 "Strings"
  */
 
 char *_strcat(char *dest, char *src)
-	
+	{
+		int i, a;
+
+		for (a = 0; dest[i] != '\0'; i++)
+		{
+		}
+		for (a = 0; src[a] != '\0'; a++)
+		{
+			dest[i] = src[a];
+			i++;
+		}
+		dest[i] = '\0';
+		return (dest);
+	}
