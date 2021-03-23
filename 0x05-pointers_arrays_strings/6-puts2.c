247f861685ce9f9b772553b47820c33beb16df28
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-23 03:37:00 +0000

    Added a second variable

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,12 @@
 
 void puts2(char *str)
 {
-int i;
+int i, t;
 
-for (i = 0; str[i] != '\0'; i = i + 2)
+for (t = 0; str[t] != '\0'; t++)
+{
+}
+for (i = 0; i < t; i = i + 2)
 {
     _putchar(str[i]);
 }
