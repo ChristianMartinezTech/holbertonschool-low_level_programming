c07f83f5b0d78ac4cc103cea1599e16d96de37f6
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-24 01:42:30 +0000

    Coded 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - check the code for Holberton School students.
+ * @dest: var
+ * @src: var
+ * @n: var
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+	dest[a] = src[a];
+	for (; a < n; a++)
+	dest[a] = '\0';
+	return (dest);
+}
