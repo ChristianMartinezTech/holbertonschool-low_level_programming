24e41fc72f2a7bd7bcb315ef07f05d494516d944
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-24 01:20:45 +0000

    Fixed some for looping issues

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 		for (b = 0; b < n && src[b] != '\0'; b++)
 		{
 		dest[a + b] = src[b];
-		dest[a + b] = '\0';
 		}
+		dest[a + b] = '\0';
 		return (dest);
 }
