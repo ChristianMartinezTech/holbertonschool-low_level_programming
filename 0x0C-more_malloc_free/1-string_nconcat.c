6d1fb2b727c349fcde76658df13305b494b90c2d
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-25 01:25:58 +0000

    Started coding 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+
+/**
+ * string_nconcat - writes the character c to stdout
+ * @s1: var
+ * @s2: var
+ * @n: var
+ * Return: char
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
