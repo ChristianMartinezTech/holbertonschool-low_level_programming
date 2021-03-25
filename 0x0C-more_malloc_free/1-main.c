22b4cf3d52ab17c2b0864d0ffda632afdb65cbb2
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-25 03:03:15 +0000

    Coded 1, waiting for Betty feedback

---
+++ 0x0C-more_malloc_free/1-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *concat;
+
+    concat = string_nconcat("Holberton ", "School !!!", 6);
+    printf("%s\n", concat);
+    free(concat);
+    return (0);
+}
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,15 +1,49 @@
 #include "holberton.h"
 
-
 /**
- * string_nconcat - writes the character c to stdout
+ * string_nconcat - concatenate strings
  * @s1: var
  * @s2: var
  * @n: var
- * Return: char
+ * Return: pointer.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *final;
+	unsigned int a, b, a2, b2;
 
+	if (s1 == '\0')
+	s1 = "";
+
+	if (s2 == '\0')
+	s2 = "";
+
+		for (a = 0; s1[a] != '\0'; a++)
+		{
+		}
+		for (b = 0; s2[b] != '\0'; b++)
+		{
+		}
+
+		if (n < b)
+			b = n;
+
+		b = (a + b);
+		final = malloc(sizeof(char) * b++);
+
+		if (final == '\0')
+			return ('\0');
+
+		for (a2 = 0; a2 < a; a2++)
+			final[a2] = s1[a2];
+		for (b2 = 0; b2 < b; b2++)
+			{
+			final[a2] = s2[b2];
+			a2++;
+			}
+			a2++;
+			final[a2] = '\0';
+
+		return (final);
 }
