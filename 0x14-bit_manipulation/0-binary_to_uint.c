69d190a2f7a712c6d6924eb15614075ff63f9819
ChristianMartinezTech <2388@holbertonschool.com>
2021-03-25 22:07:55 +0000

    First try for my program

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary to an unsignd int.
+ * @b: pointer to the array.
+ * Return: unsignd int.
+*/
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int count, a;
+unsigned int c = 1;
+unsigned int d = 0;
+
+for (count = 0; b[count] != '\0'; count++)
+{
+	a = (count - 1);
+
+	if (b[a] != 48 && b[a] != 49)
+	{
+		return (0);
+	}
+	if (b[a] == 49)
+	{
+		d = (d + c);
+	}
+	c = (c * 2);
+}
+return (d);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,8 @@
+This is the folder for the "0x14. C - Bit manipulation" Holberton proyect.
+
+Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+General
+Look for the right source of information without too much help
+How to manipulate bits and use bitwise operators
