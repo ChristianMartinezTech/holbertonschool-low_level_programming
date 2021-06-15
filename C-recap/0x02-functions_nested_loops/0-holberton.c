13152c3aad8ce47e9a6758c2f65aa413cb03777e
ChristianMartinezTech <crismartine.e@gmail.com>
2021-06-15 15:26:54 -0500

    Coded 0 and 1

---
+++ C-recap/0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main -  program that prints Holberton, followed by a new line
+ * @_putchar: writes ouput to stdout
+ * Return: 0
+ */
+
+int main(void)
+{
+    char text = "Holberton";
+    _putchar(text);
+    return(0);
+}
---
+++ C-recap/0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+* main - function that prints the alphabet, in lowercase, followed by a new line
+* 
+* Return: 0
+*/
+
+void print_alphabet(void)
+    {
+
+    }
