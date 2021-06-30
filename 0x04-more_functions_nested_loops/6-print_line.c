de80801f8e42a9daf64c7d8bf34d0c05d6c4267e
ChristianMartinezTech <crismartine.e@gmail>
2021-06-29 19:05:34 -0500

    Coded 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main -  function that draws a straight line in the terminal n times
+ * @print_line - print n number of lines
+ * Return: n times "_"
+ */
+
+void print_line(int n)
+{
+    int count;
+
+    if (n > '0')
+    {
+        for (count = '0'; count <= n; count++)
+        {
+            _putchar('_');
+            }
+    }
+    _putchar('\n');
+}
