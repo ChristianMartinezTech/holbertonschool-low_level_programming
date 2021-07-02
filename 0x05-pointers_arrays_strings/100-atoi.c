cdbf89070a34da097767ff1e051a0a4c0ecfae58
ChristianMartinezTech <crismartine.e@gmail>
2021-07-02 18:10:39 -0500

    Coded 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * 100-atoi - function that convert a string to an integer.
+ * @s: pointer to the value we give the function in main
+ * Return (0) if *s is pointing to a NULL byte
+ */
+
+int _atoi(char *s)
+{
+	char *a;
+
+	if (s[0] != NULL)
+	{
+
+	}
+	else
+	{
+		return (0)
+	}
+}
