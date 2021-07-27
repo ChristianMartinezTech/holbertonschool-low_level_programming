c77b5c1e67b589c2d5b60c1c7c9758cee0a24df9
ChristianMartinezTech <crismartine.e@gmail>
2021-07-27 15:05:16 -0500

    Coded 9

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -15,7 +15,7 @@ void print_line(int n)
         for (count = '0'; count <= n; count++)
         {
             _putchar('_');
-            }
+        }
     }
     _putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the numbers from 1 to 100 and replace multiples
+ *
+ * Return (0) - Success
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num < 100; num++)
+	{
+		if ((num % 15) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+
+		else if ((num % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	printf("Buzz\n");
+
+	return 0;
+}
