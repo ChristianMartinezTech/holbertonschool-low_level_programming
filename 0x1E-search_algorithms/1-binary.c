c9f8906005b484308ee5ca9c08491be84cfc65b4
ChristianMartinezTech <crismartine.e@gmail.com>
2022-01-19 11:28:02 -0500

    Working on 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of ints
+ * using Linear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: first index where value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t indx = 0;
+
+	if ((array == NULL) | (size == 0))
+		return (-1);
+
+	{
+		return (0);
+	}
+}
