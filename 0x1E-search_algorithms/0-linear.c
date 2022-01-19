ced42779de420979dc81d51963f72308fd453913
ChristianMartinezTech <crismartine.e@gmail.com>
2022-01-18 19:56:44 -0500

    Coded 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,7 +13,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t indx = 0;
 
-	if (array == NULL | size == 0)
+	if ((array == NULL) | (size == 0))
 		return (-1);
 
 	for (; indx < size; indx++)
